87e3394975d964d45d2a6ce5e9f0d7e1796c6c50
Alexcontreas1098 <alex-contreras@uiowa.edu>
2019-12-06 09:50:19 -0600

    added to util

--- utilities.py
+++ utilities.py
@@ -1,1 +1,12 @@
 class fin_plan:
+    def __init__(self):
+        self.income = 0
+        self.expenses = 0
+        self.expenses_list = []
+        self.expense_name = []
+        self.savingsgoal = savingsgoal
+        self.riskappetite
+
+    def aftertaxincome(self):
+        income = int(input('Enter yearly income before tax: '))
+        
